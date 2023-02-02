f2e7ef9dc528d9f0c1ef87ab213e737d76850e86
Gabiru-cpu <gabriel_librande@hotmail.com>
2023-02-02 08:36:27 -0300

    add Curso e Mentoria

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(4);
+
+        System.out.println(curso1);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Curso java");
+        mentoria1.setDescricao("descrição curso java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
